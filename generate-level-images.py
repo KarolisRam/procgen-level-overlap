094ac6e57333496b0da3c5c8e33ec6292bc616cb
KarolisRam <karolis.ram@gmail.com>
2023-09-09 11:43:42 +0100

    move saving dupes to measure-duplicates.py as an option; generate both centered and uncentered images

--- generate-level-images.py
+++ generate-level-images.py
@@ -4,26 +4,26 @@ import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
 
-CENTER = True
-if CENTER:
-    PATH = '../seeds/'
-else:
-    PATH = '../seeds-center/'
 ENVS = ['bigfish', 'bossfight', 'caveflyer', 'chaser', 'climber', 'coinrun', 'dodgeball', 'fruitbot', 'heist', 'jumper',
         'leaper', 'maze', 'miner', 'ninja', 'plunder', 'starpilot']
 DIFFS = ['easy', 'hard']
 
 
 def main():
-    for DIFF in DIFFS:
-        for ENV in ENVS:
-            os.makedirs(os.path.join(PATH, DIFF, ENV), exist_ok=True)
-            for seed in tqdm(range(200000)):
-                env = gym.make(f'procgen:procgen-{ENV}-v0', start_level=seed, num_levels=1, distribution_mode=DIFF,
-                               center_agent=CENTER)
-                obs = env.reset()
-                plt.imsave(os.path.join(PATH, DIFF, ENV, f'seed-{seed:08}.png'), obs)
-                env.close()
+    for CENTER in [True, False]:
+        if CENTER:
+            path = '../seeds/'
+        else:
+            path = '../seeds-uncenter/'
+        for DIFF in DIFFS:
+            for ENV in ENVS:
+                os.makedirs(os.path.join(path, DIFF, ENV), exist_ok=True)
+                for seed in tqdm(range(200000)):
+                    env = gym.make(f'procgen:procgen-{ENV}-v0', start_level=seed, num_levels=1, distribution_mode=DIFF,
+                                   center_agent=CENTER)
+                    obs = env.reset()
+                    plt.imsave(os.path.join(path, DIFF, ENV, f'seed-{seed:08}.png'), obs)
+                    env.close()
 
 
 if __name__ == '__main__':
--- measure-duplicates.py
+++ measure-duplicates.py
@@ -1,13 +1,15 @@
-import os
 import hashlib
+import os
+import shutil
 from tqdm import tqdm
 
 PATHS = ['../seeds/',
-         '../seeds-center/']
+         '../seeds-uncenter/']
 ENVS = ['bigfish', 'bossfight', 'caveflyer', 'chaser', 'climber', 'coinrun', 'dodgeball', 'fruitbot', 'heist', 'jumper',
         'leaper', 'maze', 'miner', 'ninja', 'plunder', 'starpilot']
 DIFFS = ['easy', 'hard']
 TQDM = False
+SAVE_DUPES = 100  # How many overlapping images to save. 0 to disable
 
 
 def compute_md5_hash(img_path):
@@ -15,7 +17,7 @@ def compute_md5_hash(img_path):
         return hashlib.md5(f.read()).hexdigest()
 
 
-def count_overlaps(directory, first_x=100000):
+def count_overlaps(directory, first_x=100000, save_dupes=100):
     hashes_seen = set()
 
     # Sort image paths alphabetically
@@ -35,9 +37,17 @@ def count_overlaps(directory, first_x=100000):
 
     # Check overlaps in the second half
     overlaps = 0
+    out_dir = os.path.join('../dupes', directory[3:])
+    if save_dupes > 0:
+        os.makedirs(out_dir, exist_ok=True)
+
     for img_path in tqdm(second_half, disable=not TQDM):
         img_hash = compute_md5_hash(img_path)
         if img_hash in hashes_seen:
+            if overlaps < save_dupes:
+                fname = img_path.split('/')[-1]
+                out_path = os.path.join(out_dir, fname)
+                shutil.copy(img_path, out_path)
             overlaps += 1
 
     return overlaps
@@ -46,11 +56,11 @@ def count_overlaps(directory, first_x=100000):
 if __name__ == "__main__":
     for PATH in PATHS:
         print(PATH)
-        # for first_x in [100000]:
-        for first_x in [1, 10, 100, 1000, 10000, 100000]:
+        for first_x in [100000]:
+        # for first_x in [1, 10, 100, 1000, 10000, 100000]:
             for DIFF in DIFFS:
                 for ENV in ENVS:
                     directory = os.path.join(PATH, DIFF, ENV)
-                    overlaps = count_overlaps(directory, first_x=first_x)
+                    overlaps = count_overlaps(directory, first_x=first_x, save_dupes=SAVE_DUPES)
                     print(f"Overlapping images {DIFF}-{ENV}, {first_x} training levels: {overlaps/100000*100:.10f}%")
         print()
